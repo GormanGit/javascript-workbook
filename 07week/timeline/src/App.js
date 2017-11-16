6974b74a1f264ef353fa3ca71c698cebb25c51fe
gormangit <contact@jongorman.top>
2017-11-16 16:44:43 -0600

    kinda working

--- 07week/timeline/src/App.js
+++ 07week/timeline/src/App.js
@@ -3,44 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-    constructor() {
-    super();
-    this.timelineData =
+    state = {
+
+    timelineData:
             [
-            {name: 'renee', post: 'I love kitties', id: 1},
-            {name: 'stevie', post: 'I love dogs', id: 2},
-            {name: 'groot', post: 'I Am Groot', id: 3},
-            {name: 'luke', post: 'blah blah blah', id: 4},
-            {name: 'vader', post: 'I am your father', id: 5},
-            {name: 'lea', post: 'Pizza', id: 6},
-            {name: 'tamra', post: 'Workout', id: 7},
-            {name: 'vicky', post: 'Coto Insurance', id: 8},
-        ];
+            {name: 'Name: renee', post: 'I love kitties', id: 1},
+            {name: 'Name: stevie', post: 'I love dogs', id: 2},
+            {name: 'Name: groot', post: 'I Am Groot', id: 3},
+            {name: 'Name: luke', post: 'blah blah blah', id: 4},
+            {name: 'Name: vader', post: 'I am your father', id: 5},
+            {name: 'Name: lea', post: 'Pizza', id: 6},
+            {name: 'Name: tamra', post: 'Workout', id: 7},
+            {name: 'Name: vicky', post: 'Coto Insurance', id: 8},
+        ]
+
+    };
+    newState={
+
+            count: 0
+
     }
-
-
-renderPosts=(e)=>{
-        return this.App.timlineData.map((name, post) => {
-            return <div> <p>{post}</p> <p>{name}</p> <a>like</a></div>
-    });
+handleClick = () => {
+this.setState({count: this.newState.count +++ 1})
+        console.log(this.newState.count)
 }
-  render() {
+renderPosts=()=> {
+    return this.state.timelineData.map((item) => {
+        return (
+            <div>
+                <p>{item.name}</p> <p>{item.post}</p>
+                <a onClick={this.handleClick}><button>like</button></a>
+                <p>{this.newState.count}</p>
+            </div>
+        );
+    })
 
-
-    return (
-
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-          {this.renderPosts()}
-      </div>
-    );
-  }
 }
+    render()  {
+        return this.renderPosts();
+    }
+}
+
 
 export default App;
+
+
