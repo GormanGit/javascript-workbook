08ccd998e03ad46fc78cf732d015adf7a6f3f6e9
gormangit <contact@jongorman.top>
2017-11-29 21:52:17 -0600

    first commit with working pages

---
+++ 08week/newapp/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage'
+import './App.css';
+// import Flexbox from 'flexbox-react';
+// import RaisedButton from 'material-ui/RaisedButton';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
+
+
+
+class App extends Component {
+
+  render() {
+    return (
+      //  this is a semantic-ui-reactcontainer that auto margins
+      <div>
+           {/*forwardslash "/" signifies the home or homepage for the path */}
+        <Route path="/" exact component={HomePage} />
+          {/*this must start with a forward slash as it signifies the homepage starting point
+          this path name does not matter as long as it matches the link*/}
+        <Route path="/login" exact component={LoginPage} />
+      </div>
+    );
+  }
+}
+
+export default App;
